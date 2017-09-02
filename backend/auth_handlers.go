722d4d486311fde71c18c75b520f5833e48ec510
Jason Deutsch <jndeutsch@gmail.com>
2017-09-02 15:44:15 -0400

    split handlers into seperate files

---
+++ backend/auth_handlers.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jasondeutsch/previ/backend/data"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"strconv"
+)
+
+type authReponse struct {
+	status  string
+	message string
+}
+
+type AuthRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// POST
+// /auth
+func authenticate(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	currentRequest := AuthRequest{}
+
+	err := json.NewDecoder(r.Body).Decode(&currentRequest)
+
+	if err != nil {
+		http.Error(w, err.Error(), 401)
+	}
+
+	user, err := data.UserByEmail(currentRequest.Email)
+
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+	}
+
+	fmt.Println(user.Password)
+	fmt.Println(data.Encrypt(currentRequest.Password))
+
+	if user.Password == data.Encrypt(currentRequest.Password) {
+
+		_, err := user.CreateSession()
+		if err != nil {
+			// do stuff
+		}
+		cookie := http.Cookie{
+			Name:     "_cookie",
+			Value:    strconv.Itoa(user.Id), // may need something better her, UUID?
+			HttpOnly: true,
+		}
+		http.SetCookie(w, &cookie)
+		w.Write([]byte("authorized\n"))
+
+	} else {
+
+		w.Write([]byte("not authorized\n"))
+	}
+
+}
+
+// POST
+// /signup
+func signup(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	var user *data.User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		return
+	}
+
+	err = user.Create()
+
+	m := map[string]interface{}{"success": err == nil, "message": "user created", "data": ""}
+
+	w.Header().Set("Contnet-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(m)
+}
+
+// POST
+// /logout
+func logout(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	cookie, err := r.Cookie("_cookie")
+
+	if err != http.ErrNoCookie {
+		id, _ := strconv.Atoi(cookie.Value)
+
+		session := data.Session{Id: id}
+		session.Delete()
+		fmt.Fprintf(w, "success")
+	}
+	// handle no cookie
+}
