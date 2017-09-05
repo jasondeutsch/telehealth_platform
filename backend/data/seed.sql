3d5e924c98aac4e022321ebf7c1dd1f933c6e35e
Jason Deutsch <jndeutsch@gmail.com>
2017-09-05 19:26:20 -0400

    formatting

--- backend/data/seed.sql
+++ backend/data/seed.sql
@@ -20,21 +20,16 @@ insert into user_account(email, password) values('j@email.com', '823d289b1c06a79
 insert into user_account(email, password, role) values('k@email.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8','{"admin"}');  
 /* ^^^ password is password */
 
-
-
 insert into patient(id, first_name, last_name, state, country) values(1,'Jason', 'Deutsch', 'Austin', 'TX');
 insert into patient(id, first_name, last_name, state, country) values(2,'Mary', 'Smith', 'Austin', 'TX');
 insert into patient(id, first_name, last_name, state, country) values(3,'David', 'Fez', 'Austin', 'TX');
 insert into patient(id, first_name, last_name, state, country) values(4,'Jason', 'Deutsch', 'Austin', 'TX');
 insert into patient(id, first_name, last_name, state, country) values(5,'Mary', 'Smith', 'Austin', 'TX');
 
-
-
 insert into provider(id, first_name, last_name, phone_number, credential) values(6,'Amy', 'Pieczarka', '5555555555', 'RD');
 insert into provider(id, first_name, last_name, phone_number, credential) values(7,'Jane','Doe','555555555','MD');
 insert into provider(id, first_name, last_name, phone_number, credential) values(8,'John','Smith','5555555555','DO');
 insert into provider(id, first_name, last_name, phone_number, credential) values(9,'Thomas','Gekkly','5555555555','PhD');
-
 
 insert into pairing(patient, provider) values(1,6);
 insert into pairing(patient, provider) values(1,7);
