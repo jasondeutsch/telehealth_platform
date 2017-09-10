57d566db62bfce5ca9b9d3d6afac7f1731450844
Jason Deutsch <jndeutsch@gmail.com>
2017-09-10 13:51:28 -0400

    cleanup

--- backend/data/admin.go
+++ backend/data/admin.go
@@ -2,22 +2,3 @@ package data
 
 // Pairing, unpair, cancel account.
 // view patient profile patients surveys, documentation, appointment history
-
-// create provider
-//func (admin *Admin) CreateProvider(u User, credential string) (err error) {
-//	statement := "insert into provider(id, credential) values($1, $2)"
-//	stmt, err := Db.Prepare(statement)
-//
-//	if err != nil {
-//		return
-//	}
-//
-//	defer stmt.Close()
-//
-//	Db.QueryRow(u.Id)
-//
-//}
-
-// get provider by id
-
-// super admin to create admins
