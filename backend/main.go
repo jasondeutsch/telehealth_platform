e301a144f3786fdbc1928b0e7e7c763ae50fd1dc
Jason Deutsch <jndeutsch@gmail.com>
2017-09-14 21:34:26 -0400

    commented out routes for availabilities and pairings

--- backend/main.go
+++ backend/main.go
@@ -24,10 +24,20 @@ func main() {
 
 	r.HandleFunc("/provider/appointments", indexAppointment).Methods("GET")
 	r.HandleFunc("/provider/appointments/{id}", showAppointment).Methods("GET")
-	r.HandleFunc("/provider/appointments/update", updateAppointment).Methods("POST").Headers("Content_type", "application/json")
+	r.HandleFunc("/provider/appointments/update", updateAppointment).Methods("POST").Headers("Content-Type", "application/json")
 	r.HandleFunc("/provider/appointments/create", createAppointment).Methods("POST").Headers("Content-Type", "application/json")
 	r.HandleFunc("/provider/appointments/{id}/cancel", cancelAppointment).Methods("PUT")
 	r.HandleFunc("/provider/appointments/{id}/complete", completeAppointment).Methods("PUT")
+
+	//r.HandleFunc("/provider/availabilities", indexAvailabilities).Methods("GET")
+	//r.HandleFunc("/provider/availabilities/create", createAvailabilities).Methods("POST")
+	//r.HandleFunc("/provider/availabilities/update", updateAvailabilities).Methods("PUT")
+
+	//r.HandleFunc("/pairings", indexPairings).Methods("GET")
+	//r.HandleFunc("/parings/{id}", showPairing).Methods("GET")
+	//r.HandleFunc("/pairings/create", createPairing).Methods("POST")
+	//r.HandleFunc("/parings/{id}/update", updatePairing).Methods("PUT")
+	//r.HandleFunc("/pairings/{id}", deletePairing).Methods("DELETE")
 
 	//Auth API
 	r.HandleFunc("/signup", signup).Methods("POST").Headers("Content-Type", "application/json")
